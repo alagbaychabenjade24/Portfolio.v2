17dea1059a0f7491a56d96adb3412a06ac2dfaea
Chaben Jade Alagbay <alagbaychabenjade24@gmail.com>
2020-11-08 21:19:51 +0800

    finished media queries for small devices

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,4 @@
 import React from 'react';
-import gmail from './icons8-gmail-min.svg';
-import github from './icons8-github-min.svg';
-import linkedin from './icons8-linkedin-min.svg';
-import heart from './icons8-heart-outline-min.svg';
-import coffee from './icons8-coffee-script-min.svg';
 
 import './Footer.scss';
 import './Footer-media.scss';
@@ -12,10 +7,22 @@ function Footer() {
     return (
         <div className='footer'>
             <div className='footer__copyright'>
-                <span>
-                    2020 &copy; Made with <img src={heart} alt='heart shape' />{' '}
-                    & <img src={coffee} alt='coffee' /> in Cebu.
-                </span>
+                <div className='top'>
+                    <span>Manly Developed by</span>
+                </div>
+
+                <div className='bottom'>
+                    <span className='name'>
+                        <a
+                            href='https://github.com/alagbaychabenjade24'
+                            target='_blank'
+                            rel='noreferrer'
+                        >
+                            Chaben Alagbay{' '}
+                        </a>
+                    </span>
+                    🤟
+                </div>
             </div>
 
             <div className='footer--btns'>
@@ -41,32 +48,6 @@ function Footer() {
                     rel='noreferrer'
                 >
                     LINKEDIN
-                </a>
-            </div>
-
-            <div className='footer__small--btns'>
-                <a
-                    href='https://github.com/alagbaychabenjade24'
-                    target='_blank'
-                    rel='noreferrer'
-                >
-                    <img src={github} alt='github' />
-                </a>
-
-                <a
-                    href='mailto:alagbaychabenjade24@gmail.com'
-                    target='_blank'
-                    rel='noreferrer'
-                >
-                    <img src={gmail} alt='gmail' />
-                </a>
-
-                <a
-                    href='https://www.linkedin.com/in/chabenjadealagbay/'
-                    target='_blank'
-                    rel='noreferrer'
-                >
-                    <img src={linkedin} alt='linkedin' />
                 </a>
             </div>
         </div>
