ed3e3be392f38a908231564608bbf7d701d392b9
Chaben Jade Alagbay <alagbaychabenjade24@gmail.com>
2020-11-12 10:59:03 +0800

    completed media queries

--- src/components/about-me/AboutMe.js
+++ src/components/about-me/AboutMe.js
@@ -5,56 +5,54 @@ import './AboutMe.scss';
 import './AboutMe-media.scss';
 
 function AboutMe() {
-    return (
-        <section className='aboutMe'>
-            <div className='aboutMe__title'>
-                <h1>About Me</h1>
-            </div>
+	return (
+		<section className='aboutMe'>
+			<div className='aboutMe__title'>
+				<h1>About Me</h1>
+			</div>
 
-            <div className='aboutMe__content'>
-                <p className='aboutMe__content__intro'>
-                    I'm currently improving my coding skills. I've also designed
-                    websites as my side projects to enhance my knowledge to
-                    write readable code and improve my design skills since I'm
-                    specializing in front-end development.
-                </p>
+			<div className='aboutMe__content'>
+				<p className='aboutMe__content__intro'>
+					I'm currently improving my coding skills. I've also designed
+					websites as my side projects to enhance my knowledge to write
+					readable code and improve my design skills since I'm specializing
+					in front-end development.
+				</p>
 
-                <p className='aboutMe__content__experience'>
-                    I'm a part-time IT Instructor for Japanese students, where I
-                    help them learn how to write code and create websites that
-                    they can show off. Our lessons range from the foundations of
-                    web design to the deployment of a full-scale website using
-                    modern technology.
-                </p>
+				<p className='aboutMe__content__experience'>
+					I'm a part-time IT Instructor for Japanese students, where I help
+					them learn how to write code and create websites that they can
+					show off. Our lessons range from the foundations of web design to
+					the deployment of a full-scale website using modern technology.
+				</p>
 
-                <p className='aboutMe__content__mission'>
-                    As a front-end developer, my goal is to improve every day
-                    for me to create the most efficient and scalable websites
-                    possible. I make it my mission to continue growing and be a
-                    better person than I was yesterday.
-                </p>
+				<p className='aboutMe__content__mission'>
+					As a front-end developer, my goal is to improve every day for me
+					to create the most efficient and scalable websites possible. I
+					make it my mission to continue growing and be a better person
+					than I was yesterday.
+				</p>
 
-                <p className='aboutMe__content__interests'>
-                    <span className='aboutMe__interests--bold'>
-                        When I'm not coding
-                    </span>
-                    , I'm most likely learning how to improve the way I write my
-                    code, create side projects, and watch movies or listen to
-                    musics.
-                </p>
+				<p className='aboutMe__content__interests'>
+					<span className='aboutMe__interests--bold'>
+						When I'm not coding
+					</span>
+					, I'm most likely learning how to improve the way I write my
+					code, create side projects, and watch movies or listen to musics.
+				</p>
 
-                <div className='aboutMe__btn--resume'>
-                    <a
-                        href='https://drive.google.com/file/d/1OpS2XfM_Oup72Ib_llwEgMNA2SioH4nT/view?usp=sharing'
-                        target='_blank'
-                        rel='noreferrer'
-                    >
-                        View My Resume <img src={blueArrow} alt='blue arrow' />
-                    </a>
-                </div>
-            </div>
-        </section>
-    );
+				<div className='aboutMe__btn--resume'>
+					<a
+						href='https://drive.google.com/file/d/1OpS2XfM_Oup72Ib_llwEgMNA2SioH4nT/view?usp=sharing'
+						target='_blank'
+						rel='noreferrer'
+					>
+						View My Resume <img src={blueArrow} alt='blue arrow' />
+					</a>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default AboutMe;
